708b62ffbfabdbf0921f28e9b64aa3b4016c98c0
Antor <afakhrul9@gmail.com>
2021-06-26 02:52:59 +0600

    -Home and Class Activity added

---
+++ app/src/main/java/com/siddiqei/liveclassai/ui/classroom/ClassActivity.kt
@@ -0,0 +1,11 @@
+package com.siddiqei.liveclassai.ui.classroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class)
+    }
+}
